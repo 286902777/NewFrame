e66373e63f9824ee54b9a0dbe0987ef73965b035
frame-ios <>
2026-01-04 21:32:18 -0500

    fix 1.2.0

--- lib/controller/set_page.dart
+++ lib/controller/set_page.dart
@@ -174,6 +174,7 @@ class _SetPageState extends State<SetPage>
         openEmail();
       // MobileAds.instance.openAdInspector((error) {});
       case 1:
+        print("ssss");
         Get.to(
           () => (WebPage(name: '', link: 'https://frameplayvid.com/terms/')),
         );
